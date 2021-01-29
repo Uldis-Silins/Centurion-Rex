deb75a0a782676db908215830a1a9d7682f9c135
Uldis-Silins <dissid@inbox.lv>
2021-01-29 22:27:18 +0200

    spritesort

---
+++ Assets/Scripts/IsoSprite.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer)), ExecuteInEditMode]
+public class IsoSprite : MonoBehaviour
+{
+    private SpriteRenderer m_spriteRenderer;
+
+    private void Awake()
+    {
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+        //transform.localScale += Scale();
+    }
+
+    private void Update()
+    {
+        m_spriteRenderer.sortingOrder = (int)transform.position.z * -10;
+    }
+
+    private Vector3 Scale()
+    {
+        return new Vector3(Mathf.Atan(Mathf.Sin(Camera.main.transform.eulerAngles.x * Mathf.Deg2Rad)),
+            Mathf.Atan(Mathf.Sin(Camera.main.transform.eulerAngles.x * Mathf.Deg2Rad)),
+            transform.localScale.z);
+    }
+}
