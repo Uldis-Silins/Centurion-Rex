93ffba622f9f506e8b636897523600a0e037819f
Uldis-Silins <dissid@inbox.lv>
2021-01-29 18:39:15 +0200

    refactor of unit

---
+++ Assets/Scripts/Units/Unit_Base.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Unit_Base : MonoBehaviour
+{
+    [SerializeField] protected SpriteRenderer m_soldierRenderer;
+
+    protected Camera m_mainCam;
+
+    protected virtual void Awake()
+    {
+        m_mainCam = Camera.main;
+    }
+
+    protected virtual void LateUpdate()
+    {
+        // billboard
+        m_soldierRenderer.transform.rotation = m_mainCam.transform.rotation;
+    }
+}
---
+++ Assets/Scripts/Units/Unit_Peditatus.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit_Peditatus : Unit_Base, ISelecteble
+{
+    public SelectableManager selectableManager;
+
+    private Color m_startColor;
+    private readonly int m_colorPropID = Shader.PropertyToID("_Color");
+
+    public bool IsSelected { get; private set; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        m_startColor = m_soldierRenderer.material.GetColor(m_colorPropID);
+    }
+
+    private void Start()
+    {
+        selectableManager.RegisterSelectable(this, gameObject);
+        Debug.Log("Registering " + name);
+    }
+
+    protected override void LateUpdate()
+    {
+        base.LateUpdate();
+    }
+
+    //private void OnDisable()
+    //{
+    //    selectableManager.UnregisterSelectable(this);
+    //}
+
+    public void Select()
+    {
+        IsSelected = true;
+        m_soldierRenderer.material.SetColor(m_colorPropID, Color.green);
+    }
+
+    public void Deselect()
+    {
+        IsSelected = false;
+        m_soldierRenderer.material.SetColor(m_colorPropID, m_startColor);
+    }
+}
