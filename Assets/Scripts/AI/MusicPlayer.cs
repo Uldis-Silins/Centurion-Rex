befeab9e837e4d26f79f283fdcaa2815890b8022
Uldis-Silins <dissid@inbox.lv>
2021-01-31 09:42:00 +0200

    muzons

---
+++ Assets/Scripts/AI/MusicPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioSource menuMusic;
+    public AudioSource gameMusic;
+
+    private float m_fadeTimer;
+    private readonly float m_fadeTime = 2f;
+
+    private GameState m_curGameState;
+    private bool m_isFading;
+
+    private void Awake()
+    {
+        //DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (Player_Controller.currentGameState != m_curGameState)
+        {
+            m_curGameState = Player_Controller.currentGameState;
+            m_fadeTimer = 0f;
+            m_isFading = true;
+        }
+
+        if (m_isFading)
+        {
+            m_fadeTimer += Time.unscaledDeltaTime;
+
+            if (m_curGameState == GameState.Playing)
+            {
+                gameMusic.volume = Mathf.Lerp(0f, 1f, m_fadeTimer / m_fadeTime);
+                menuMusic.volume = Mathf.Lerp(1f, 0f, m_fadeTimer / m_fadeTime);
+            }
+            else
+            {
+                gameMusic.volume = Mathf.Lerp(1f, 0f, m_fadeTimer / m_fadeTime);
+                menuMusic.volume = Mathf.Lerp(0f, 1f, m_fadeTimer / m_fadeTime);
+                Debug.Log("Should change vol; timer: " + m_fadeTimer);
+            }
+
+            if (m_fadeTimer >= m_fadeTime)
+            {
+                m_isFading = false;
+            }
+        }
+    }
+}
