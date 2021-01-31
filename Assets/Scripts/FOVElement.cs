9479ec4fc89cbb7036a18a1694ca40f84f3cbf00
Uldis-Silins <dissid@inbox.lv>
2021-01-31 15:07:29 +0200

    building fov

---
+++ Assets/Scripts/FOVElement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FOVElement : MonoBehaviour
+{
+    public float visionDistance;
+
+    public bool CanSeeUnit(Unit_Base unit)
+    {
+        return Vector3.Distance(transform.position, unit.transform.position) <= visionDistance;
+    }
+}
