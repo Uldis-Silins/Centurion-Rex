3413c0d05eac4a93df72d10f908e7d18be77ab00
Sacristan <girts.kesteris@gmail.com>
2021-05-17 23:35:46 +0300

    console fow on/off

--- Assets/Console.cs
+++ Assets/Console.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Sacristan.Ahhnold.Core;
 
 public class Console : Sacristan.Ahhnold.Runtime.Console
@@ -8,6 +6,7 @@ public class Console : Sacristan.Ahhnold.Runtime.Console
     public override CommandRegistration[] RegistrableCommands => new CommandRegistration[] {
         new CommandRegistration("version", VersionAction, "Outputs game version"),
         new CommandRegistration("quit", QuitAction, "Quit Game"),
+        new CommandRegistration("fow", FowAction, "Fog of War on/off")
     };
 
     #region Command handlers
@@ -25,6 +24,38 @@ public class Console : Sacristan.Ahhnold.Runtime.Console
     static void VersionAction(string[] args)
     {
         ConsoleController.Log(string.Format("version: {0}", Application.version));
+    }
+
+    static void FowAction(string[] args)
+    {
+        if (args.Length == 0 || string.IsNullOrEmpty(args[0]))
+        {
+            ConsoleController.LogError("missing parameter");
+        }
+
+        else if (args.Length > 1)
+        {
+            ConsoleController.LogError("more than 1 parameters");
+        }
+        else
+        {
+            string val = args[0].ToLower();
+            bool on = val == "on";
+            bool off = val == "off";
+
+            if (on || off)
+            {
+                Projector darkProjector = GameObject.Find("FogCameraContainer/DarkProjector").GetComponent<Projector>(); //TODO: not nice
+                darkProjector.enabled = on;
+
+                if (on) ConsoleController.Log("Fog of War ON");
+                else ConsoleController.Log("Fog of War OFF");
+            }
+            else
+            {
+                ConsoleController.LogError("param1 should be on or off");
+            }
+        }
     }
 
     #endregion
